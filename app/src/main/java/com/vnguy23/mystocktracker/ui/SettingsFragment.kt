484ab93f72801f8aa8c634c1dc17d2bc718ca9e9
Vinh Nguyen <vinh28062002@gmail.com>
2022-04-19 00:35:30 -0400

    4

--- app/src/main/java/com/vnguy23/mystocktracker/ui/SettingsFragment.kt
+++ app/src/main/java/com/vnguy23/mystocktracker/ui/SettingsFragment.kt
@@ -1,17 +1,22 @@
 package com.vnguy23.mystocktracker.ui
 
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.vnguy23.mystocktracker.databinding.FragmentSettingsBinding
 
+
 class SettingsFragment : Fragment() {
 
-
+    private lateinit var prefs: SharedPreferences
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
 
+    @SuppressLint("CommitPrefEdits")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,6 +24,56 @@ class SettingsFragment : Fragment() {
         setHasOptionsMenu(true)
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         binding.apply {
+            prefs = PreferenceManager.getDefaultSharedPreferences(binding.root.context)
+            val editor: SharedPreferences.Editor = prefs.edit()
+
+            if(prefs.getInt("CARD", 1) == 1){
+                GreyCard.isChecked = true
+            }else{
+                RedCard.isChecked = true
+            }
+
+            if(prefs.getInt("CODE", 1) == 1){
+                GreenCode.isChecked = true
+            }else{
+                YellowCode.isChecked = true
+            }
+
+            if(prefs.getInt("INPUT", 1) == 1){
+                BlueInput.isChecked = true
+            }else{
+                PurpleInput.isChecked = true
+            }
+
+            CardSettingGroup.setOnCheckedChangeListener { _, _ ->
+                if(GreyCard.isChecked){
+                    editor.putInt("CARD", 1)
+                    editor.apply()
+                } else{
+                    editor.putInt("CARD", 2)
+                    editor.apply()
+                }
+            }
+            CodeSettingGroup.setOnCheckedChangeListener { _, _ ->
+                if(GreenCode.isChecked){
+                    editor.putInt("CODE", 1)
+                    editor.apply()
+                } else{
+                    editor.putInt("CODE", 2)
+                    editor.apply()
+                }
+            }
+            InputSettingGroup.setOnCheckedChangeListener { _, _ ->
+                if(BlueInput.isChecked){
+                    editor.putInt("INPUT", 1)
+                    editor.apply()
+                } else{
+                    editor.putInt("INPUT", 2)
+                    editor.apply()
+                }
+            }
+
+
             settingsDoneButton.setOnClickListener {
                 findNavController().navigate(SettingsFragmentDirections.actionSettingsFragmentToMainFragment())
             }

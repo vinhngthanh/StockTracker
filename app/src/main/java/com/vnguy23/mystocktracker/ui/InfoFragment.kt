3180dca382dac9d2f314be2b334aedb9f67e7026
Vinh Nguyen <vinh28062002@gmail.com>
2022-04-18 21:00:33 -0400

    3

--- app/src/main/java/com/vnguy23/mystocktracker/ui/InfoFragment.kt
+++ app/src/main/java/com/vnguy23/mystocktracker/ui/InfoFragment.kt
@@ -3,10 +3,9 @@ package com.vnguy23.mystocktracker.ui
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.vnguy23.mystocktracker.R
 import com.vnguy23.mystocktracker.databinding.FragmentInfoBinding
-import com.vnguy23.mystocktracker.databinding.FragmentSettingsBinding
+
 
 private var _binding: FragmentInfoBinding? = null
 private val binding get() = _binding!!
@@ -16,6 +15,7 @@ class InfoFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+
         }
     }
 
@@ -26,7 +26,12 @@ class InfoFragment : Fragment() {
         setHasOptionsMenu(true)
         _binding = FragmentInfoBinding.inflate(inflater, container, false)
         binding.apply {
+            val applicationInfo = requireContext().applicationInfo
+            val stringId = applicationInfo.labelRes
+            appName.setText(context?.getString(stringId) ?: String())
 
+            val versionName = requireContext().packageManager.getPackageInfo(requireContext().packageName, 0).versionName
+            appVersion.text = getString(R.string.version, versionName)
         }
         return binding.root
     }
